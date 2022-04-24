e8506c23bdc0e03b47fffad0bc1e42a881bcde55
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-04-24 11:20:52 +0900

    feat: implement verifier.sol

--- contracts/ProofOfDfWinner.sol
+++ contracts/ProofOfDfWinner.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "../circuits/verifier.sol";
 
 // is Verifier, ERC721Mintable
 contract ProofOfDfWinner is ERC721 {
@@ -10,18 +11,21 @@ contract ProofOfDfWinner is ERC721 {
     Counters.Counter private _tokenIds;
     mapping(bytes => bool) public nullifiers;
 
-    function mint(bytes calldata proof, bytes calldata nullifier) external {
-        // verify proof
-        // check that nullifier hasn't been used
-        require(nullifiers[nullifier] != true);
-        // mint NFT
+    function mint(
+        uint256[2] memory _a,
+        uint256[2][2] memory _b,
+        uint256[2] memory _c,
+        uint256[1] memory _input,
+        uint256 _nullifier) external {
+        
+        require(nullifiers[_nullifier] == false, "nullifier has been already used.");
+        require(Verifier.verifyProof(_a, _b, _c, _input), "invalid proof");
+        nullifiers[_nullifier] = true;
+
         _tokenIds.increment();
         uint256 tokenId = _tokenIds.current();
         _mint(msg.sender, tokenId);
-        nullifiers[nullifier] = true;
     }
 
-    constructor() ERC721("Proof", "PROF") {
-        // no time
-    }
+    constructor() ERC721("Proof", "PROF") {}
 }

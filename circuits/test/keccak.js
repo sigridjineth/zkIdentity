a2a7731cf43d82959348051b6ad373bbe5ed0e7b
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-04-24 16:02:47 +0900

    style: format using prettier

--- circuits/test/keccak.js
+++ circuits/test/keccak.js
@@ -1,119 +1,134 @@
-const path = require("path")
-const chai = require("chai")
-const circom_tester = require("circom_tester")
+const path = require("path");
+const chai = require("chai");
+const circom_tester = require("circom_tester");
 
 const assert = chai.assert;
 const wasm_tester = circom_tester.wasm;
 
 function bytesToBits(b) {
-    const bits = [];
-    for (let i = 0; i < b.length; i++) {
-      for (let j = 0; j < 8; j++) {
-        if ((Number(b[i])&(1<<j)) > 0) {
-          // bits.push(Fr.e(1));
-          bits.push(1);
-        } else {
-          // bits.push(Fr.e(0));
-          bits.push(0);
-        }
+  const bits = [];
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < 8; j++) {
+      if ((Number(b[i]) & (1 << j)) > 0) {
+        // bits.push(Fr.e(1));
+        bits.push(1);
+      } else {
+        // bits.push(Fr.e(0));
+        bits.push(0);
       }
     }
-    return bits
+  }
+  return bits;
 }
-  
-  function bitsToBytes(a) {
-    const b = [];
-  
-    for (let i=0; i<a.length; i++) {
-      const p = Math.floor(i/8);
-      if (b[p] == undefined) {
-        b[p] = 0;
-      }
-      if (a[i] == 1) {
-        b[p] |= 1<<(i%8);
-      }
+
+function bitsToBytes(a) {
+  const b = [];
+
+  for (let i = 0; i < a.length; i++) {
+    const p = Math.floor(i / 8);
+    if (b[p] == undefined) {
+      b[p] = 0;
     }
-    return b;
+    if (a[i] == 1) {
+      b[p] |= 1 << i % 8;
+    }
+  }
+  return b;
 }
 
 function hexToBytes(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
-      bytes.push(parseInt(hex.substr(c, 2), 16));
+    bytes.push(parseInt(hex.substr(c, 2), 16));
   return bytes;
 }
 
 function bytesToHex(bytes) {
   for (var hex = [], i = 0; i < bytes.length; i++) {
-      var current = bytes[i] < 0 ? bytes[i] + 256 : bytes[i];
-      hex.push((current >>> 4).toString(16));
-      hex.push((current & 0xF).toString(16));
+    var current = bytes[i] < 0 ? bytes[i] + 256 : bytes[i];
+    hex.push((current >>> 4).toString(16));
+    hex.push((current & 0xf).toString(16));
   }
   return hex.join("");
 }
 
 describe("Keccak256 32 bytes input and output test", function () {
-    this.timeout(100000);
+  this.timeout(100000);
 
-    let circuit;
+  let circuit;
 
-    before(async() => {
-        circuit = await wasm_tester(path.join(__dirname, "circuits", "keccak_256_256.circom"));
-        await circuit.loadConstraints();
-        
-        console.log("n_constraints", circuit.constraints.length);
-    })
+  before(async () => {
+    circuit = await wasm_tester(
+      path.join(__dirname, "circuits", "keccak_256_256.circom")
+    );
+    await circuit.loadConstraints();
 
-    it("testvector generated from go keccak 1 test", async() => {
-        const input = [116, 101, 115, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        const expectedOut = [37, 17, 98, 135, 161, 178, 88, 97, 125, 150, 143,
-            65, 228, 211, 170, 133, 153, 9, 88, 212, 4, 212, 175, 238, 249,
-            210, 214, 116, 170, 85, 45, 21];
-        
-        const inIn = bytesToBits(input);
+    console.log("n_constraints", circuit.constraints.length);
+  });
 
-        const witness = await circuit.calculateWitness({
-            "in": inIn,
-        }, true);
+  it("testvector generated from go keccak 1 test", async () => {
+    const input = [
+      116, 101, 115, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+    const expectedOut = [
+      37, 17, 98, 135, 161, 178, 88, 97, 125, 150, 143, 65, 228, 211, 170, 133,
+      153, 9, 88, 212, 4, 212, 175, 238, 249, 210, 214, 116, 170, 85, 45, 21,
+    ];
 
-        const stateOut = witness.slice(1, 1 + (32 * 8));
-        const stateOutBytes = bitsToBytes(stateOut);
+    const inIn = bytesToBits(input);
 
-        assert.deepEqual(stateOutBytes, expectedOut);
-    })
-})
+    const witness = await circuit.calculateWitness(
+      {
+        in: inIn,
+      },
+      true
+    );
 
-describe("keccak 64bytes input, 32bytes output which is PubKey to Address test", function() {
-    this.timeout(100000);
-    
-    let circuit;
+    const stateOut = witness.slice(1, 1 + 32 * 8);
+    const stateOutBytes = bitsToBytes(stateOut);
 
-    before(async() => {
-        circuit = await wasm_tester(path.join(__dirname, "circuits", "keccak_512_256.circom"));
-        await circuit.loadConstraints();
-    });
+    assert.deepEqual(stateOutBytes, expectedOut);
+  });
+});
 
-    it("should properly hashes an Ethereum public key to its keccak hash", async() => {
-        // given
-        const pubKey = "11f2b30c9479ccaa639962e943ca7cfd3498705258ddb49dfe25bba00a555e48cb35a79f3d084ce26dbac0e6bb887463774817cb80e89b20c0990bc47f9075d5";
-        const address = "f2c712fa067b0bb7f35a89ecc2524c08e0166f6a3b8d9925f8864c8ee18cb729";
+describe("keccak 64bytes input, 32bytes output which is PubKey to Address test", function () {
+  this.timeout(100000);
 
-        // when
-        const inBits = bytesToBits(hexToBytes(pubKey));
-        const witness = await circuit.calculateWitness({
-          "in": inBits
-        }, true);
+  let circuit;
 
-        console.log("inBits: ", inBits);
-        console.log("witness: ", witness);
+  before(async () => {
+    circuit = await wasm_tester(
+      path.join(__dirname, "circuits", "keccak_512_256.circom")
+    );
+    await circuit.loadConstraints();
+  });
 
-        const outBits = witness.slice(1, 1 + (32 * 8));
-        const outHex = bytesToHex(bitsToBytes(outBits));
+  it("should properly hashes an Ethereum public key to its keccak hash", async () => {
+    // given
+    const pubKey =
+      "11f2b30c9479ccaa639962e943ca7cfd3498705258ddb49dfe25bba00a555e48cb35a79f3d084ce26dbac0e6bb887463774817cb80e89b20c0990bc47f9075d5";
+    const address =
+      "f2c712fa067b0bb7f35a89ecc2524c08e0166f6a3b8d9925f8864c8ee18cb729";
 
-        console.log("outBits: ", outBits);
-        console.log("outHex: ", outHex);
+    // when
+    const inBits = bytesToBits(hexToBytes(pubKey));
+    const witness = await circuit.calculateWitness(
+      {
+        in: inBits,
+      },
+      true
+    );
 
-        // then
-        assert.equal(outHex, address);
-    })
-})
+    console.log("inBits: ", inBits);
+    console.log("witness: ", witness);
+
+    const outBits = witness.slice(1, 1 + 32 * 8);
+    const outHex = bytesToHex(bitsToBytes(outBits));
+
+    console.log("outBits: ", outBits);
+    console.log("outHex: ", outHex);
+
+    // then
+    assert.equal(outHex, address);
+  });
+});
--- circuits/test/utils.js
+++ circuits/test/utils.js
@@ -1,57 +1,65 @@
 function bytesToBits(b) {
-    const bits = [];
-    for (let i = 0; i < b.length; i++) {
-      for (let j = 0; j < 8; j++) {
-        if ((Number(b[i])&(1<<j)) > 0) {
-          // bits.push(Fr.e(1));
-          bits.push(1);
-        } else {
-          // bits.push(Fr.e(0));
-          bits.push(0);
-        }
+  const bits = [];
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < 8; j++) {
+      if ((Number(b[i]) & (1 << j)) > 0) {
+        // bits.push(Fr.e(1));
+        bits.push(1);
+      } else {
+        // bits.push(Fr.e(0));
+        bits.push(0);
       }
     }
-    return bits
   }
-  
-  function bitsToBytes(a) {
-    const b = [];
-  
-    for (let i=0; i<a.length; i++) {
-      const p = Math.floor(i/8);
-      if (b[p]==undefined) {
-        b[p] = 0;
-      }
-      if (a[i]==1) {
-        b[p] |= 1<<(i%8);
-      }
+  return bits;
+}
+
+function bitsToBytes(a) {
+  const b = [];
+
+  for (let i = 0; i < a.length; i++) {
+    const p = Math.floor(i / 8);
+    if (b[p] == undefined) {
+      b[p] = 0;
     }
-    return b;
+    if (a[i] == 1) {
+      b[p] |= 1 << i % 8;
+    }
   }
-  
-  function hexToBytes(hex) {
-      for (var bytes = [], c = 0; c < hex.length; c += 2)
-          bytes.push(parseInt(hex.substr(c, 2), 16));
-      return bytes;
+  return b;
+}
+
+function hexToBytes(hex) {
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
+function bytesToHex(bytes) {
+  for (var hex = [], i = 0; i < bytes.length; i++) {
+    var current = bytes[i] < 0 ? bytes[i] + 256 : bytes[i];
+    hex.push((current >>> 4).toString(16));
+    hex.push((current & 0xf).toString(16));
   }
-  
-  function bytesToHex(bytes) {
-      for (var hex = [], i = 0; i < bytes.length; i++) {
-          var current = bytes[i] < 0 ? bytes[i] + 256 : bytes[i];
-          hex.push((current >>> 4).toString(16));
-          hex.push((current & 0xF).toString(16));
-      }
-      return hex.join("");
+  return hex.join("");
+}
+
+const numToBits = (num, numBits) => {
+  const bits = (num >>> 0).toString(2);
+  const padding = numBits - bits.length;
+  const paddedBits = padding > 0 ? bits.padStart(numBits, 0) : bits;
+
+  if (numBits < bits.length) {
+    throw new Error("Not enough bits");
   }
-  
-  function numToBits(num, numBits) {
-    // TODO: n-bit precision for a single num
-  }
-  
-  module.exports = {
-    bytesToBits,
-    bitsToBytes,
-    hexToBytes,
-    bytesToHex,
-    numToBits
-  };
+
+  return paddedBits.split("");
+};
+
+module.exports = {
+  bytesToBits,
+  bitsToBytes,
+  hexToBytes,
+  bytesToHex,
+  numToBits,
+};

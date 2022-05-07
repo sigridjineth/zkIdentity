972f867b1a99f9eb7849f011f2ae59500b737b96
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-05-07 22:07:55 +0900

    refactor: add accounts

--- test/identity-test.js
+++ test/identity-test.js
@@ -1,14 +1,20 @@
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 
-const words = "abcdefghijklmnopqrstuvwxyz";
-const arr = words.split("");
+const accounts = ["0x0f75f25eb9bc68c8886d6d4828966c58aac3c232",
+"0x8459c6bebe2d53b4dcaa71499a1ae4274c0e4df9",
+"0x509c39f7a55666fdc2ac90b085b39b41d0f089a0",
+"0xc415040996590a6eb82ebb2b323b3fae84268e5d",
+"0xdcaf829700b68df3ddde26f27313b8328debd9cb"];
+
+// const arr = words.split("");
 // console.log(arr);
 
 let identityCommitments = [];
-for (let i = 0; i < arr.length; i++) {
+
+for (let i = 0; i < accounts.length; i++) {
   identityCommitments.push(
     BigInt(
-      new ZkIdentity(Strategy.MESSAGE, arr[i]).genIdentityCommitment()
+      new ZkIdentity(Strategy.MESSAGE, accounts[i]).genIdentityCommitment()
     ).toString()
   );
 }

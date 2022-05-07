1c35a539ee1198972a5d5aab242fef6671d6eb71
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-05-07 22:53:24 +0900

    test: createIdentityCommitments succeed

--- test/identity-test.js
+++ test/identity-test.js
@@ -1,23 +1,41 @@
-import { Strategy, ZkIdentity } from "@zk-kit/identity"
+import { Strategy, ZkIdentity } from "@zk-kit/identity";
 
-const accounts = ["0x0f75f25eb9bc68c8886d6d4828966c58aac3c232",
-"0x8459c6bebe2d53b4dcaa71499a1ae4274c0e4df9",
-"0x509c39f7a55666fdc2ac90b085b39b41d0f089a0",
-"0xc415040996590a6eb82ebb2b323b3fae84268e5d",
-"0xdcaf829700b68df3ddde26f27313b8328debd9cb"];
+const accounts = [
+  "0x0f75f25eb9bc68c8886d6d4828966c58aac3c232",
+  "0x8459c6bebe2d53b4dcaa71499a1ae4274c0e4df9",
+  "0x509c39f7a55666fdc2ac90b085b39b41d0f089a0",
+  "0xc415040996590a6eb82ebb2b323b3fae84268e5d",
+  "0xdcaf829700b68df3ddde26f27313b8328debd9cb",
+];
 
 // const arr = words.split("");
 // console.log(arr);
 
-let identityCommitments = [];
+function createIdentityCommitments() {
+  let identityCommitments = [];
 
-for (let i = 0; i < accounts.length; i++) {
-  identityCommitments.push(
-    BigInt(
-      new ZkIdentity(Strategy.MESSAGE, accounts[i]).genIdentityCommitment()
-    ).toString()
-  );
+  for (let i = 0; i < accounts.length; i++) {
+    identityCommitments.push(
+      BigInt(
+        new ZkIdentity(Strategy.MESSAGE, accounts[i]).genIdentityCommitment()
+      ).toString()
+    );
+  }
+
+  return identityCommitments;
 }
 
-console.log(JSON.stringify(identityCommitments));
-console.log("Array length is", identityCommitments.length);
+export default createIdentityCommitments;
+
+// let identityCommitments = [];
+
+// for (let i = 0; i < accounts.length; i++) {
+//   identityCommitments.push(
+//     BigInt(
+//       new ZkIdentity(Strategy.MESSAGE, accounts[i]).genIdentityCommitment()
+//     ).toString()
+//   );
+// }
+
+// console.log(JSON.stringify(identityCommitments));
+// console.log("Array length is", identityCommitments.length);
--- test/index.ts
+++ test/index.ts
@@ -1,34 +1,38 @@
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
 import { expect } from "chai"
-import { Contract, Signer } from "ethers"
+import { Contract } from "ethers"
 import { ethers, run } from "hardhat"
-import identityCommitments from "../public/identityCommitments.json"
+// import identityCommitments from "../public/identityCommitments.json"
+import createIdentityCommitments from "../test/identity-test"
 
 describe("Greeters", function () {
     let contract: Contract
-    let contractOwner: Signer
-    let dontSure: Signer
+    // let contractOwner: Signer
+    // let dontSure: Signer
 
     before(async () => {
         contract = await run("deploy", { logs: false })
 
-        const signers = await ethers.getSigners()
-        contractOwner = signers[0]
-        dontSure = signers[1]
+        // const signers = await ethers.getSigners()
+        // contractOwner = signers[0]
+        // dontSure = signers[1]
     })
 
     describe("# greet", () => {
-        const wasmFilePath = "./public/semaphore.wasm"
-        const finalZkeyPath = "./public/semaphore_final.zkey"
+        const wasmFilePath = "../public/semaphore.wasm"
+        const finalZkeyPath = "../public/semaphore_final.zkey"
 
         it("Should greet", async () => {
-            const message = await contractOwner.signMessage("Sign this message to create your identity!")
+            // const message = await contractOwner.signMessage("Sign this message to create your identity!")
+            const message = "0x0f75f25eb9bc68c8886d6d4828966c58aac3c232";
 
             const identity = new ZkIdentity(Strategy.MESSAGE, message)
-            const identityCommitment = identity.genIdentityCommitment()
+            const identityCommitment = BigInt(identity.genIdentityCommitment()).toString();
             const greeting = "Hello world"
             const bytes32Greeting = ethers.utils.formatBytes32String(greeting)
+
+            const identityCommitments = createIdentityCommitments();
 
             const merkleProof = generateMerkleProof(20, BigInt(0), identityCommitments, identityCommitment)
             const witness = Semaphore.genWitness(
@@ -39,56 +43,29 @@ describe("Greeters", function () {
                 greeting
             )
 
-            const witness2 = Semaphore.genWitness(
-                identity.getTrapdoor() + 1n,
-                identity.getNullifier() + 1n,
-                merkleProof,
-                merkleProof.root,
-                greeting
-            )
+            // const witness2 = Semaphore.genWitness(
+            //     identity.getTrapdoor() + 1n,
+            //     identity.getNullifier() + 1n,
+            //     merkleProof,
+            //     merkleProof.root,
+            //     greeting
+            // )
 
             const fullProof = await Semaphore.genProof(witness, wasmFilePath, finalZkeyPath)
             const solidityProof = Semaphore.packToSolidityProof(fullProof.proof)
 
-            const fullProof2 = await Semaphore.genProof(witness2, wasmFilePath, finalZkeyPath)
-            const solidityProof2 = Semaphore.packToSolidityProof(fullProof2.proof)
+            // const fullProof2 = await Semaphore.genProof(witness2, wasmFilePath, finalZkeyPath)
+            // const solidityProof2 = Semaphore.packToSolidityProof(fullProof2.proof)
 
             const nullifierHash = Semaphore.genNullifierHash(merkleProof.root, identity.getNullifier())
 
-            const transaction2 = contract.greet(bytes32Greeting, nullifierHash, solidityProof2)
+            // const transaction2 = contract.greet(bytes32Greeting, nullifierHash, solidityProof2)
 
-            await expect(transaction2).to.emit(contract, "NewGreeting").withArgs(bytes32Greeting)
+            // await expect(transaction2).to.emit(contract, "NewGreeting").withArgs(bytes32Greeting)
 
             const transaction = contract.greet(bytes32Greeting, nullifierHash, solidityProof)
             await expect(transaction).to.emit(contract, "NewGreeting").withArgs(bytes32Greeting)
 
-        }),
-
-        it("could greet? ", async () => {
-            const message = await dontSure.signMessage("Sign this message to create your identity!")
-
-            const identity = new ZkIdentity(Strategy.MESSAGE, message)
-            const identityCommitment = identity.genIdentityCommitment()
-            const greeting = "Hello world"
-            const bytes32Greeting = ethers.utils.formatBytes32String(greeting)
-
-            const merkleProof = generateMerkleProof(20, BigInt(0), identityCommitments, identityCommitment)
-            const witness = Semaphore.genWitness(
-                identity.getTrapdoor(),
-                identity.getNullifier(),
-                merkleProof,
-                merkleProof.root,
-                greeting
-            )
-
-            const fullProof = await Semaphore.genProof(witness, wasmFilePath, finalZkeyPath)
-            const solidityProof = Semaphore.packToSolidityProof(fullProof.proof)
-
-            const nullifierHash = Semaphore.genNullifierHash(merkleProof.root, identity.getNullifier())
-
-            const transaction = contract.greet(bytes32Greeting, nullifierHash, solidityProof)
-
-            await expect(transaction).to.emit(contract, "NewGreeting").withArgs(bytes32Greeting)
         })
     })
 })
